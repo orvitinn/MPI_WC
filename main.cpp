7d3d7749b752d0021453bdfd281dd8533a06e5ea
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2013-11-26 17:47:05 +0000

    bla

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include <sstream>
 #include <cctype>
 #include <algorithm>
-#include <boost/algorithm/string.hpp>
 #include <ctype.h>
 #include <typeinfo>
 #include <sstream>
@@ -24,84 +23,19 @@ using std::string;
 
 void mapper();
 void partitioner();
-void reducer();
+extern void reducer(MPI_Comm communicator, const string& filename);
 
-void split(const std::string &s, char delim, std::vector<std::string> &elems) {
-    std::stringstream ss(s);
-    std::string item;
-    while(std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-}
-
-inline std::string trim(const std::string &s)
-{
-    auto wsfront=std::find_if_not(s.begin(),s.end(),[](int c){return std::isspace(c);});
-    auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
-    return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
-}
-
-
-void readFile(MPI_Comm communicator, char filename[]){
-    int overlap = 20;
-    int nodechucksize=200;
-    int loopoffset, readoverlap;
-    int new_rank, new_size;
-    vector<std::string> lines;
-    std::vector<std::string> words;
-    MPI_File fh;
-    MPI_Status status;
-    
-    MPI_Comm_rank(communicator,&new_rank);
-    MPI_Comm_size(communicator,&new_size);
-    int rc = MPI_File_open( communicator, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-    MPI_Offset  filesize;
-    MPI_File_get_size(fh, &filesize);
-    
-    int chunksize = nodechucksize * sizeof(char);
-    loopoffset = new_size*nodechucksize;
-    readoverlap = overlap * sizeof(char);
-    vector<char> text_buffer(chunksize+overlap);
-    for (int i=0; i < 3; i++){
-        MPI_File_set_view(fh,(new_rank*sizeof(char)*nodechucksize+(loopoffset*i)),MPI_CHAR,MPI_CHAR,"native",MPI_INFO_NULL);
-        MPI_File_read(fh, &text_buffer[0], chunksize+readoverlap, MPI_CHAR, &status);
-        printf("read processor rank %i read : ",new_rank);
-        std::string text_string_buffer(text_buffer.begin(), text_buffer.end());
-        split(text_string_buffer, '\n', lines);
-        # pragma omp for
-        for(string &line: lines){
-            std::stringstream ss;
-            int first=0;
-            for (char &stafur: line){
-                if(isalpha(stafur) && first == 0)
-                    continue;
-                
-                if(!isalpha(stafur) && first == 0){
-                    first = 1;
-                    continue;
-                }
-                
-                if(isalpha(stafur))
-                    ss << stafur;
-                
-                if(!isalpha(stafur) && ss.str().length() > 0){
-                    cout << "ord : " << ss.str() << endl;
-                    ss.str(std::string());
-                }
-            }
-        }
-        cout << endl;
-    }
-    MPI_File_close(&fh);
-    
-}
+const int MAX_MAPP_ID = 2;
+const int MAX_PART_ID = 4;
+int overlap = 20;
+int nodechucksize=200;
 
 
 int main(int argc, char* argv[]) {
     MPI_Status status;
     int rank, size, color, errs=0;
     double START;
-    char readfilename[] = "pg100.txt";
+    string readfilename = "/home/maa33/code/mpi_wc/pg100.txt";
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
@@ -113,33 +47,36 @@ int main(int argc, char* argv[]) {
     
     // búum til com fyrir hópana
     MPI_Group mappers, partitioners, reducers, everyone;
-    MPI_Comm partararcomm, reddararcomm;
+    MPI_Comm mapparacom, partararcomm, reddararcomm;
 
-    vector<int> partarar = {0, 1};
-    vector<int> reddarar = {2, 3};
+    vector<int> mapparar = {0, 1};
+    vector<int> partarar = {2, 3};
+    vector<int> reddarar = {4, 5};
     
     MPI_Comm_group(MPI_COMM_WORLD, &everyone);
 
     // ákveðum
     
+    MPI_Group_incl(everyone, 2, &mapparar[0], &mappers);
+    MPI_Comm_create(MPI_COMM_WORLD, mappers, &mapparacom);
+    
     MPI_Group_incl(everyone, 2, &partarar[0], &partitioners);
     MPI_Comm_create(MPI_COMM_WORLD, partitioners, &partararcomm);
+
+    MPI_Group_incl(everyone, 2, &reddarar[0], &reducers);
+    MPI_Comm_create(MPI_COMM_WORLD, reducers, &reddararcomm);
     
-    MPI_Group_incl(everyone, 2, &reddarar[0], &mappers);
-    MPI_Comm_create(MPI_COMM_WORLD, mappers, &reddararcomm);
+    cout << "DONE creating groups and comms" << endl;
     
-    if (rank > 1)
-        readFile(reddararcomm,readfilename);
-    
-    cout << "DONE" << endl;
-    
-    if (rank < 2)
+    if (rank < MAX_MAPP_ID)
     {
-        //simulateElevator(rank, partararcomm);
+        reducer(mapparacom,readfilename);
+    }
+    else if (rank < MAX_PART_ID)
+    {
     }
     else
     {
-        //simulateFloor(rank, reddararcomm);
     }
     
     cout << "Node " << rank << " stopping." << endl;
