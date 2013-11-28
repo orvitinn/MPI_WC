44db5788e36b25cb7aa6a3668cedd4dc5797c839
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2013-11-28 23:14:05 +0000

    dev..

--- main.cpp
+++ main.cpp
@@ -23,27 +23,32 @@ using std::string;
 
 extern void mapper(MPI_Comm communicator, int rank, const string& filename);
 extern void reducer(MPI_Comm communicator, int rank);
+extern void collector(int rank);
 
 const int MAX_MAPP_ID = 2;
 const int MAX_PART_ID = 4;
 int overlap = 20;
-int nodechucksize=200;
+int nodechucksize=20000;
+int collector_rank;
 
 vector<int> mapparar = {0, 1};
-vector<int> partarar = {2, 3};
-vector<int> reddarar = {4, 5};
+vector<int> reddarar = {2, 3};
+vector<char> reddarar_range_start = {'a','m', 127};
 
 int main(int argc, char* argv[]) {
     int rank, size;
     string readfilename = "/home/maa33/code/mpi_wc/pg100.txt";
+    // string readfilename = "/home/maa33/code/mpi_wc/smaller.txt";
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
+    collector_rank = size-1; // sá síðasti er kollektorinn
+    
     // búum til com fyrir hópana
-    MPI_Group mappers, partitioners, reducers, everyone;
-    MPI_Comm mapparacom, partararcomm, reddararcomm;
+    MPI_Group mappers, reducers, everyone;
+    MPI_Comm mapparacom, reddararcomm;
 
     
     MPI_Comm_group(MPI_COMM_WORLD, &everyone);
@@ -53,9 +58,6 @@ int main(int argc, char* argv[]) {
     MPI_Group_incl(everyone, 2, &mapparar[0], &mappers);
     MPI_Comm_create(MPI_COMM_WORLD, mappers, &mapparacom);
     
-    MPI_Group_incl(everyone, 2, &partarar[0], &partitioners);
-    MPI_Comm_create(MPI_COMM_WORLD, partitioners, &partararcomm);
-
     MPI_Group_incl(everyone, 2, &reddarar[0], &reducers);
     MPI_Comm_create(MPI_COMM_WORLD, reducers, &reddararcomm);
     
@@ -65,14 +67,16 @@ int main(int argc, char* argv[]) {
     {
         mapper(mapparacom, rank, readfilename);
     }
-    /*
-    else if (rank < MAX_PART_ID)
+    else if (rank < size-1)
     {
         reducer(reddararcomm, rank);
     }
-     */
-    // einn process er í gather lógík, fær niðurstöður frá öllum reducers
+    else
+    {
+        collector(rank);
+    }
     
+    // einn process er í gather lógík, fær niðurstöður frá öllum reducers
     cout << "Node " << rank << " stopping." << endl;
     MPI_Barrier(MPI_COMM_WORLD);
     cout << "Node " << rank << " stopped." << endl;
